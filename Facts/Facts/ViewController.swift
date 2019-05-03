9d6726dfeda73061a37b7fb78a6240d94d30df3c
Julio Fernandes <julio.junior@xpi.com.br>
2019-05-03 00:12:54 -0300

    Adicionando gitignore e criando projeto base

---
+++ Facts/Facts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facts
+//
+//  Created by Julio Fernandes on 02/05/19.
+//  Copyright © 2019 Julio Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
